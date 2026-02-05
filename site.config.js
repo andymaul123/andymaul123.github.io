440c51c9ba0e00e49aed68ac3f37526699943d65
Andrew Maul <andymaul123@gmail.com>
2026-02-04 22:21:40 -0800

    New post: learning soft pastels. First post using automated scripts!

--- site.config.js
+++ site.config.js
@@ -1,13 +1,11 @@
-module.exports = 
-
-{
+module.exports = {
   build: {
     srcPath: './src',
     outputPath: './public'
   },
   site: {
     title: 'Andrew Maul',
-    year: '2026',
-    latest: '2025-11-08_recursive-probability'
+    year: 2026,
+    latest: '2026-02-04_learning-soft-pastels'
   }
 };
